c13104663825cee64a4be8c9972b8a6a7efbefe2
RaccoonCode96 <waawoo13@gmail.com>
2021-01-29 23:42:19 +0900

    Study_Django02

---
+++ fc_community/fcuser/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Fcuser(models.Model):
+    username = models.CharField(max_length=32, verbose_name='사용자명')
+    password = models.CharField(max_length=64, verbose_name='비밀번호')
+    registered_dttm = models.DateTimeField(
+        auto_now_add=True, verbose_name='등록시간')
+
+    class Meta:
+        db_table = 'fastcampus_fcuser'
