f13204452bb30c9a34ef41f42c7d7cef00e664ee
RaccoonCode96 <waawoo13@gmail.com>
2021-02-03 02:29:57 +0900

    study django04

--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -5,9 +5,16 @@ from django.db import models
 
 class Fcuser(models.Model):
     username = models.CharField(max_length=32, verbose_name='사용자명')
+    useremail = models.EmailField(
+        max_length=128, verbose_name='사용자 이메일')  # Email 형태 검증 까지 해줌
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(
         auto_now_add=True, verbose_name='등록시간')
 
+    def __str__(self):
+        return self.username
+
     class Meta:
         db_table = 'fastcampus_fcuser'
+        verbose_name = '패스트 캠퍼스 사용자'  # 기본적으로 복수형을 지원하기 때문에 s가 자동으로 붙게 되어 있음
+        verbose_name_plural = '패스트 캠퍼스 사용자'  # 복수형이름을 따로 지정하여 자동으로 붙는 s를 제거 가능
--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,3 +1,58 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.hashers import make_password, check_password  # 저장된 password 암호화
+from .models import Fcuser
 # Create your views here.
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        res_data = {}
+        if not (username and password):
+            res_data['error'] = '모든 값을 입력해 주세요.'
+        else:
+            fcuser = Fcuser.objects.get(username=username)
+            if check_password(password, fcuser.password):
+                # 로그인 처리
+                pass
+            else:
+                res_data['error'] = '잘못된 아이디 또는 비밀번호 입니다.'
+        return render(request, 'login.html', res_data)
+
+
+def register(request):  # request 형태로 전달해줘야 되고
+    if request.method == 'GET':
+        # 반환하고 싶은 html파일, 폴더를 포함하면 경로 모양으로
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        # username = request.POST['username'] # 값을 못받고 제출시 오류가 나기 때문에 예외 처리 필요
+        # password = request.POST['password']
+        # re_password = request.POST['re-password']
+        username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
+
+        res_data = {}
+
+        if not (username and useremail and password and re_password):  # 값을 못받는 경우 에러시 에러출력 메세지
+            res_data['error'] = '모든 값을 입력해야 합니다.'
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 일치하지 않습니다!'
+            # return HttpResponse('비밀번호가 일치하지 않습니다!')
+        else:
+            fcuser = Fcuser(
+                username=username,
+                useremail=useremail,
+                password=make_password(password)  # 암호화 함수를 사용해서 암호화 하여 생성
+            )
+
+            fcuser.save()
+
+        return render(request, 'register.html', res_data)
+# 들어오는 방법이 2가지임, 직접 주소를 작성하는 경우 및 등록버튼을 눌러서 보내는 경우
